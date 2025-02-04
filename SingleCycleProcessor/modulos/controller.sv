eb161d85be71ccb856d0fabfb6b96e44c69aad77
juanpluduena <juan.luduena.161@mi.unc.edu.ar>
2025-02-04 19:52:58 -0300

    ej1 tp3 terminado

--- SingleCycleProcessor/modulos/controller.sv
+++ SingleCycleProcessor/modulos/controller.sv
@@ -1,13 +1,18 @@
 // CONTROLLER
 
 module controller(input logic [10:0] instr,
-						output logic [3:0] AluControl,						
-						output logic reg2loc, regWrite, AluSrc, Branch,
-											memtoReg, memRead, memWrite);
+						input logic ExtIRQ, ExcAck, reset,
+						output logic [3:0] AluControl, EStatus,
+						output logic [1:0] AluSrc,
+						output logic reg2loc, regWrite, Branch,
+											memtoReg, memRead, memWrite, Exc, ERet, ExtIAck);
 											
 	logic [1:0] AluOp_s;
+	logic NotAnInstr = 1'b0;
+	logic [3:0] alu_control_res;
 											
 	maindec 	decPpal 	(.Op(instr), 
+							.reset(reset),
 							.Reg2Loc(reg2loc), 
 							.ALUSrc(AluSrc), 
 							.MemtoReg(memtoReg), 
@@ -15,11 +20,21 @@ module controller(input logic [10:0] instr,
 							.MemRead(memRead), 
 							.MemWrite(memWrite), 
 							.Branch(Branch), 
-							.ALUOp(AluOp_s));	
+							.ALUOp(AluOp_s),
+							.ERet(ERet),
+							.NotAnInstr(NotAnInstr));	
 					
 								
 	aludec 	decAlu 	(.funct(instr), 
 							.aluop(AluOp_s), 
-							.alucontrol(AluControl));
+							.alucontrol(alu_control_res));
+							
+	assign Exc = ExtIRQ | NotAnInstr;
+	
+	assign AluControl = reset ? 4'b0 : alu_control_res; 
+	
+	assign EStatus = NotAnInstr ? 4'b0010 : (ExtIRQ ? 4'b0001 : 4'b0);
+	
+	assign ExtIAck = ExcAck & ExtIRQ;
 			
 endmodule
--- SingleCycleProcessor/modulos/maindec.sv
+++ SingleCycleProcessor/modulos/maindec.sv
@@ -1,75 +1,133 @@
 module maindec(
 	input logic [10:0] Op,
-	output logic Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,
-	output logic [1:0] ALUOp
+	input logic reset,
+	output logic Reg2Loc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ERet, NotAnInstr,
+	output logic [1:0] ALUOp, ALUSrc
 );
 	always_comb begin
-		case (Op)
-			11'b111_1100_0010: begin // LDUR
-				Reg2Loc = 0;
-				ALUSrc = 1;
-				MemtoReg = 1;
-				RegWrite = 1;
-				MemRead = 1;
-				MemWrite = 0;
-				Branch = 0;
-				ALUOp = 2'b00;
-			end
+	
+		if (reset) begin
+			Reg2Loc = 1'b0;
+			ALUSrc = 2'b00;
+			MemtoReg = 1'b0;
+			RegWrite = 1'b0;
+			MemRead = 1'b0;
+			MemWrite = 1'b0;
+			Branch = 1'b0;
+			ALUOp = 2'b00;
+			ERet = 1'b0;
+			NotAnInstr = 1'b0;
+		end
+		
+		else begin
+		
+			case (Op)
+				11'b111_1100_0010: begin // LDUR
+					Reg2Loc = 1'bx;
+					ALUSrc = 2'b01;
+					MemtoReg = 1'b1;
+					RegWrite = 1'b1;
+					MemRead = 1'b1;
+					MemWrite = 1'b0;
+					Branch = 1'b0;
+					ALUOp = 2'b00;
+					ERet = 1'b0;
+					NotAnInstr = 1'b0;
+				end
+				
+				11'b111_1100_0000: begin // STUR
+					Reg2Loc = 1'b1;
+					ALUSrc = 2'b01;
+					MemtoReg = 1'bx;
+					RegWrite = 1'b0;
+					MemRead = 1'b0;
+					MemWrite = 1'b1;
+					Branch = 1'b0;
+					ALUOp = 2'b00;
+					ERet = 1'b0;
+					NotAnInstr = 1'b0;
+				end
+				
+				11'b101_1010_0000,
+				11'b101_1010_0001,
+				11'b101_1010_0010,
+				11'b101_1010_0011,
+				11'b101_1010_0100,
+				11'b101_1010_0101,
+				11'b101_1010_0110,
+				11'b101_1010_0111: begin // CBZ
+					Reg2Loc = 1'b1;
+					ALUSrc = 2'b00;
+					MemtoReg = 1'bx;
+					RegWrite = 1'b0;
+					MemRead = 1'b0;
+					MemWrite = 1'b0;
+					Branch = 1'b1;
+					ALUOp = 2'b01;
+					ERet = 1'b0;
+					NotAnInstr = 1'b0;
+				end
+				
+				11'b100_0101_1000,
+				11'b110_0101_1000,
+				11'b100_0101_0000,
+				11'b101_0101_0000: begin // ADD, SUB, AND, ORR
+					Reg2Loc = 1'b0;
+					ALUSrc = 2'b00;
+					MemtoReg = 1'b0;
+					RegWrite = 1'b1;
+					MemRead = 1'b0;
+					MemWrite = 1'b0;
+					Branch = 1'b0;
+					ALUOp = 2'b10;
+					ERet = 1'b0;
+					NotAnInstr = 1'b0;
+				end
+				
+				11'b110_1011_0100: begin // ERET
+					Reg2Loc = 1'b0;
+					ALUSrc = 2'b00;
+					MemtoReg = 1'bx;
+					RegWrite = 1'b0;
+					MemRead = 1'b0;
+					MemWrite = 1'b0;
+					Branch = 1'b1;
+					ALUOp = 2'b01;
+					ERet = 1'b1;
+					NotAnInstr = 1'b0;
+				end
+				
+				11'b110_1010_1001: begin // MRS
+					Reg2Loc = 1'b1;
+					ALUSrc = 2'b1x;
+					MemtoReg = 1'b0;
+					RegWrite = 1'b1;
+					MemRead = 1'b0;
+					MemWrite = 1'b0;
+					Branch = 1'b0;
+					ALUOp = 2'b01;
+					ERet = 1'b0;
+					NotAnInstr = 1'b0;
+				end
+				
+				default: begin // INVALID OPCODE
+					Reg2Loc = 1'bx;
+					ALUSrc = 2'bxx;
+					MemtoReg = 1'b0;
+					RegWrite = 1'b0;
+					MemRead = 1'b0;
+					MemWrite = 1'b0;
+					Branch = 1'b0;
+					ALUOp = 2'bxx;
+					ERet = 1'b0;
+					NotAnInstr = 1'b1;
+				end
+				
+			endcase
 			
-			11'b111_1100_0000: begin // STUR
-				Reg2Loc = 1;
-				ALUSrc = 1;
-				MemtoReg = 0;
-				RegWrite = 0;
-				MemRead = 0;
-				MemWrite = 1;
-				Branch = 0;
-				ALUOp = 2'b00;
-			end
-			
-			11'b101_1010_0000,
-			11'b101_1010_0001,
-			11'b101_1010_0010,
-			11'b101_1010_0011,
-			11'b101_1010_0100,
-			11'b101_1010_0101,
-			11'b101_1010_0110,
-			11'b101_1010_0111: begin // CBZ
-				Reg2Loc = 1;
-				ALUSrc = 0;
-				MemtoReg = 0;
-				RegWrite = 0;
-				MemRead = 0;
-				MemWrite = 0;
-				Branch = 1;
-				ALUOp = 2'b01;
-			end
-			
-			11'b100_0101_1000,
-			11'b110_0101_1000,
-			11'b100_0101_0000,
-			11'b101_0101_0000: begin // ADD, SUB, AND, ORR
-				Reg2Loc = 0;
-				ALUSrc = 0;
-				MemtoReg = 0;
-				RegWrite = 1;
-				MemRead = 0;
-				MemWrite = 0;
-				Branch = 0;
-				ALUOp = 2'b10;
-			end
-			
-			default: begin // default
-				Reg2Loc = 0;
-				ALUSrc = 0;
-				MemtoReg = 0;
-				RegWrite = 0;
-				MemRead = 0;
-				MemWrite = 0;
-				Branch = 0;
-				ALUOp = 2'b00;
-			end
-		endcase
+		end
+		
 	end
+	
 endmodule
 			
